f6f057f8e357c4d033efc4696a4b3f1680310856
Numericlock <49630768+Numericlock@users.noreply.github.com>
2021-04-27 06:35:49 +0900

    ListModal　更新　GROUP CONCAT

--- app/Models/Disclosure_list.php
+++ app/Models/Disclosure_list.php
@@ -10,8 +10,11 @@ class Disclosure_list extends Model
 	protected $fillable = ['name', 'owner_user_id', 'is_published', 'is_hidden'];
     
     public function scopeIndex($query,$user_id){
-        return $query->where('owner_user_id', $user_id)
+        return $query->select('disclosure_lists.*',\DB::raw("GROUP_CONCAT(disclosure_lists_users.user_id)"))
+        ->leftjoin('disclosure_lists_users','disclosure_lists_users.list_id','=','disclosure_lists.id')
+        ->where('owner_user_id', $user_id)
         ->where('is_hidden', 0)
+        ->groupBy('disclosure_lists.id')
         ->latest('updated_at')
         ->get();
     }
